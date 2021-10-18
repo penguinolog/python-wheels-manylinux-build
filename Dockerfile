8f14169bc216e41876eb77f8656a1465cb1711d7
Aleksei Stepanov <penguinolog@gmail.com>
2021-10-18 15:27:52 +0200

    Support custom builder and use setuptools by default

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM quay.io/pypa/manylinux2010_x86_64
 
-ENV PLAT manylinux2010_x86_64
+ENV PLAT=manylinux2010_x86_64\
+    PYTHONDONTWRITEBYTECODE=1
 
 COPY entrypoint.sh /entrypoint.sh
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,41 +1,57 @@
 #!/bin/bash
 set -e -x
 
+arch=$(uname -m)
+
 # CLI arguments
 PY_VERSIONS=$1
 BUILD_REQUIREMENTS=$2
 SYSTEM_PACKAGES=$3
 PRE_BUILD_COMMAND=$4
-PACKAGE_PATH=$5
-PIP_WHEEL_ARGS=$6
+BUILD_EXECUTABLE=$5
+BUILD_COMMAND=$6
+PREBUILD_PATH=$7
+PACKAGE_PATH=$8
 
 # Temporary workaround for LD_LIBRARY_PATH issue. See
 # https://github.com/RalfG/python-wheels-manylinux-build/issues/26
 export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/usr/local/lib
-
-cd /github/workspace/"${PACKAGE_PATH}"
 
 if [ ! -z "$SYSTEM_PACKAGES" ]; then
     yum install -y ${SYSTEM_PACKAGES}  || { echo "Installing yum package(s) failed."; exit 1; }
 fi
 
 if [ ! -z "$PRE_BUILD_COMMAND" ]; then
+    cd /github/workspace/"$PREBUILD_PATH"
     $PRE_BUILD_COMMAND || { echo "Pre-build command failed."; exit 1; }
 fi
+
+cd /github/workspace/"${PACKAGE_PATH}"
+
+# pre-cleanup
+rm -rf *.egg-info
+find -noleaf -name "*.py[co]" -delete
 
 # Compile wheels
 arrPY_VERSIONS=(${PY_VERSIONS// / })
 for PY_VER in "${arrPY_VERSIONS[@]}"; do
-    # Update pip
-    /opt/python/"${PY_VER}"/bin/pip install --upgrade --no-cache-dir pip
+    echo "Python ${PY_VER} ${arch}:"
+    python_bin="/opt/python/${PY_VER}/bin"
+    pip="$python_bin/pip"
+
+    # Prepare base python
+    "$pip" install -U --disable-pip-version-check pip setuptools auditwheel
 
     # Check if requirements were passed
     if [ ! -z "$BUILD_REQUIREMENTS" ]; then
-        /opt/python/"${PY_VER}"/bin/pip install --no-cache-dir ${BUILD_REQUIREMENTS} || { echo "Installing requirements failed."; exit 1; }
+        "$pip" install --no-cache-dir --disable-pip-version-check ${BUILD_REQUIREMENTS} || { echo "Installing requirements failed."; exit 1; }
     fi
 
+    # Check if environment consistent
+    "$pip" check --no-input --disable-pip-version-check
+
     # Build wheels
-    /opt/python/"${PY_VER}"/bin/pip wheel . ${PIP_WHEEL_ARGS} || { echo "Building wheels failed."; exit 1; }
+    "$python_bin/$BUILD_EXECUTABLE" $BUILD_COMMAND || { echo "Building wheels failed."; exit 1; }
 done
 
 # Bundle external shared libraries into the wheels
@@ -49,6 +65,13 @@ if [[ -f "$failed_wheels" ]]; then
     cat failed-wheels
     exit 1
 fi
+
+# Cleanup
+rm -rf .eggs
+rm -rf build
+rm -rf *.egg-info
+# Clean caches and cythonized
+find -noleaf -name "*.py[co]" -delete
 
 echo "Succesfully build wheels:"
 find . -type f -iname "*-manylinux*.whl"
